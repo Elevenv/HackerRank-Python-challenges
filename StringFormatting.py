7780cc017c376e33715a13a2a7698abb30a3bbe6
Elevenv <vitthaljadhav1420@gmail.com>
2022-04-12 14:23:49 +0530

    String Formatting bin oct hex

---
+++ StringFormatting.py
@@ -0,0 +1,28 @@
+
+# def print_formatted(n):
+#     for i in range(1,n+1):
+#         binary = bin(i)
+#         octal = oct(i)
+#         hexa = hex(i)
+#         print(i,'\t',str(octal[2:]),'\t',str(hexa[2:]),'\t',str(binary[2:]))
+        
+# if __name__ == '__main__':
+#     n = int(input())
+#     print_formatted(n)
+
+
+def print_formatted(number):
+    
+    l1 = len(bin(number)[2:])
+   
+    for i in range(1,number+1):
+        print(str(i).rjust(l1,' '),end=" ")
+        print(oct(i)[2:].rjust(l1,' '),end=" ")
+        print(((hex(i)[2:]).upper()).rjust(l1,' '),end=" ")
+        print(bin(i)[2:].rjust(l1,' '),end=" ")
+        print("")
+        
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)

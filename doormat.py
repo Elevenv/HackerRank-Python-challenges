d93792342aa2670ee5fdcf8cc1e8f07a96b77fef
Elevenv <vitthaljadhav1420@gmail.com>
2022-04-12 13:30:39 +0530

    NxM Door mat

---
+++ doormat.py
@@ -0,0 +1,12 @@
+# Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+# Mat size must be NXM. (N is an odd natural number, and M is 3 times N.)
+# The design should have 'WELCOME' written in the center.
+# The design pattern should only use |, . and - characters.
+
+N, M = map(int, input().split())
+for i in range(1, N, 2):
+    print(str('.|.' * i).center(M, '-'))
+print('WELCOME'.center(M, '-'))
+for i in range(N-2, -1, -2):
+    print(str('.|.' * i).center(M, '-'))
